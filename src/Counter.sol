8094a4aca45683b3f03e0a1583d1420deb6b2f8d
Entreprenerd <alex@entreprenerd.xyz>
2024-02-26 20:31:11 +0100

    c:4

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,7 +5,7 @@ contract Counter {
     uint128 public number;
 
     function setNumber(uint64 newNumber) public {
-        number = newNumber; // the x is
+        number = newNumber; // the x is another
     }
 
     function increment() public {
