359a28f498e6d027442b3239c98200da8c4e3e50
Entreprenerd <alex@entreprenerd.xyz>
2024-03-07 20:52:21 +0100

    chore: x

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,7 +5,7 @@ contract Counter {
     uint128 public number;
 
     function setNumber(uint64 newNumber) public {
-        number = newNumber; // Trigger a webhook
+        number = newNumber; // asd a webhook
     }
     function increment() public {
         number++;
