7d28cf052c36007483685464fdfdbaa4783117f4
Alex The Entreprenerd <alex@entreprenerd.xyz>
2024-05-09 11:05:01 +0200

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,7 +5,7 @@ contract Counter {
     uint128 public number;
 
     function setNumber(uint64 newNumber) public {
-        number = newNumber; // trigger a commit a
+        number = newNumber; // trigger a commit a asd
     }
 
     function increment() public {
