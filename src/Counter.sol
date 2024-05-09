a99499ad7fd0dc08b2ffafba2ecab96f697cfd8f
Alex The Entreprenerd <alex@entreprenerd.xyz>
2024-05-09 09:15:23 +0200

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,7 +5,7 @@ contract Counter {
     uint128 public number;
 
     function setNumber(uint64 newNumber) public {
-        number = newNumber; //  a webhook agaiasdadn asd
+        number = newNumber; //  a webhook agaiasdadn asd asd
     }
 
     function increment() public {
