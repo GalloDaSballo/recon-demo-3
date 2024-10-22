94ed7932a7cf297888186131156e88b879802f0f
Alex The Entreprenerd <alex@entreprenerd.xyz>
2024-10-22 17:46:00 +0200

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,7 +5,7 @@ contract Counter {
     uint128 public number;
 
     function setNumber(uint64 newNumber) public {
-        number = newNumber; // First UI test
+        number = newNumber; // First UI test aasd
     }
 
     function increment() public {
